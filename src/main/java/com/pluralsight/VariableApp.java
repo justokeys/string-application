8455361778717ab655469772e65065e124540066
JustinK <productivekeys@gmail.com>
2026-04-08 14:44:16 -0500

    Intial commit

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,13 +2,17 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
-        String favorateColor = "Purple";
+        String favorateColor = "Purple ";
         int birthYear = 2026;
         char middleIntial = 'J';
         boolean havePets = false;
-        String message = "Your Dreams will come true if you are DISCIPLINE!";
+        String message = " Your Dreams will come true if you are DISCIPLINE!";
 
-        System.out.println(favorateColor + " " + birthYear + " " + middleIntial + " " + havePets + message);
+        System.out.println(favorateColor  + birthYear + " " + middleIntial + " " + havePets + message);
+
+        String week = "There are 7 days in own week. ";
+        String coffee = "The price of coffe at this bouje spot is ($4.99)";
+        char favletter = 'J'
 
 
 
