d13313d1aaa7fb4f797bf74e20c66ca6b367548e
JustinK <productivekeys@gmail.com>
2026-04-08 15:04:21 -0500

    Completed Exercise 2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -6,13 +6,26 @@ public class VariableApp {
         int birthYear = 2026;
         char middleIntial = 'J';
         boolean havePets = false;
-        String message = " Your Dreams will come true if you are DISCIPLINE!";
+        String message = " Your Dreams will come true if you have DISCIPLINE!";
 
         System.out.println(favorateColor  + birthYear + " " + middleIntial + " " + havePets + message);
 
         String week = "There are 7 days in own week. ";
-        String coffee = "The price of coffe at this bouje spot is ($4.99)";
-        char favletter = 'J';
+        String coffee = "The price of coffee at this boujee spot is : $";
+        double coffeePrice = 4.99;
+        String letter = "My fav letter is ";
+        char favLetter = 'J';
+        String isRaining = "If it raining outside say its true: ";
+        boolean raining = true;
+
+
+        System.out.println(week);
+        System.out.println(coffee + coffeePrice);
+        System.out.println(letter + favLetter);
+        System.out.println(isRaining + raining);
+
+
+
 
 
 
