0c572ac53236e728149049ee42061dddb54de286
JustinK <productivekeys@gmail.com>
2026-04-08 14:36:06 -0500

    Intial commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String favorateColor = "Purple";
+        int birthYear = 2026;
+        char middleIntial = 'J';
+        boolean havePets = false;
+        String message = "Your Dreams will come true if you are DISCIPLINE!";
+
+
+    }
+}
